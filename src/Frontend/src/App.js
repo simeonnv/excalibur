8f89d055c04a7238333eda8b78f669841faed863
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2024-07-17 19:40:15 +0300

    Added 2 new pages
    Look at the comment i put in src/Frontend/src/App.js
    
    Пусни сайта и ми кажи как изглежда при теб

--- src/Frontend/src/App.js
+++ src/Frontend/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import LoginSignup from './components/assets/LoginSignup/LoginSignup';
+import LoginSignup from './Components/Assets/LoginSignup/LoginSignup';
+//import ClassroomDashboard from './Components/Assets/ClassroomDashboard/ClassroomDashboard';
+//import ClassroomDashboardTeacher from './Components/Assets/ClassroomDashboardTeacher/ClassroomDashboardTeacher';
 
+// Когато искаш да пуснеш уеб сайта, остави само импорта на уеб сайта който ти трябва. може да коментираш тези които не ти трябват
+
+// За да се Пусне уеб сайта, пишете npm start след като напишете cd src/Frontend
 
 function App() {
   return (
     <div>
-      <LoginSignup/>
-    </div>
-  );
+      <LoginSignup/> 
+    </div> // ^^^ Сменяш кои сайт да пуснеш
+  ); 
 }
 
 export default App;
