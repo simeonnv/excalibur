7b10a6c9cc46e5e7411c2eb26775cde6bcb08c84
zxxtlz <75642081+zxxtlz@users.noreply.github.com>
2024-07-24 17:24:50 +0300

    Removed some things :)))

--- src/Frontend/src/Components/Assets/ClassroomDashboardTeacher/ClassroomDashboardTeacher.jsx
+++ src/Frontend/src/Components/Assets/ClassroomDashboardTeacher/ClassroomDashboardTeacher.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import Select from "react-dropdown-select";
 import './ClassroomDashboardTeacher.css';
 
 const ClassroomDashboard = () => {
